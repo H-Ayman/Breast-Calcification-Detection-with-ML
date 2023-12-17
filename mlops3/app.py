bc4b1851726ef545940291ece2fb1e1261247c0b
haytham2872 <139249374+haytham2872@users.noreply.github.com>
2023-12-17 20:40:13 +0100

    Update app.py

--- mlops3/app.py
+++ mlops3/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, File, UploadFile
-from prometheus_fastapi_instrumentator import Instrumentator
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 import joblib
 import numpy as np
 import io
 from sklearn.preprocessing import StandardScaler
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = FastAPI()
 
@@ -13,8 +15,11 @@ model = joblib.load('knn_model.joblib')
 
 Instrumentator().instrument(app).expose(app)
 
+# Create a counter metric to track HTTP requests
+http_requests_total = Counter('http_requests_total', 'Total number of HTTP requests')
+
 # Define constants
-num_features = 5  # Update with the actual number of features in your dataset
+num_features = 5  
 
 class Item(BaseModel):
     features: list
@@ -29,3 +34,14 @@ async def predict(item: Item):
 
     prediction = model.predict(features)
     return {"prediction": int(prediction[0])}
+
+@app.get("/")
+def read_root():
+    # Increment the counter on each request
+    http_requests_total.inc()
+    return {"it's": "working"}
+
+@app.get("/metrics", response_class=PlainTextResponse)
+def metrics():
+    # Return Prometheus-compatible metrics
+    return generate_latest(http_requests_total)
